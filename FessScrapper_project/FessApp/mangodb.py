4188e1e22987ebc08ca470831f526d30e9380659
SanjeevKote <sanjeevkote.mca@gmail.com>
2024-08-23 11:15:27 +0530

    db changes

--- FessScrapper_project/FessApp/mangodb.py
+++ FessScrapper_project/FessApp/mangodb.py
@@ -2,14 +2,14 @@
 import pymongo
 
 # MongoDB connection details
-# MONGODB_HOST = '57.128.39.222' #prod server docker
-MONGODB_HOST = '54.37.76.85' #test server docker
+MONGODB_HOST = '57.128.39.222' #prod server docker
+# MONGODB_HOST = '54.37.76.85' #test server docker
 # MONGODB_HOST = 'host.docker.internal' #'scrapper-mongodb' # local docker
 # MONGODB_HOST = 'localhost' 
-# MONGODB_USERNAME = 'chiesi-prod' #PROD
-# MONGODB_PASSWORD = 'Q^1JI5t_SuE21(Ss' #PROD
-MONGODB_USERNAME = 'fess' #TEST
-MONGODB_PASSWORD = 'fess' #TEST
+MONGODB_USERNAME = 'chiesi-prod' #PROD
+MONGODB_PASSWORD = 'Q^1JI5t_SuE21(Ss' #PROD
+# MONGODB_USERNAME = 'fess' #TEST
+# MONGODB_PASSWORD = 'fess' #TEST
 MONGODB_PORT = 27017
 MONGODB_DB = 'scrapperDatabase'
 
